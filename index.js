2e32899bd6959e8f6e119497f78250d0e0fc0294
Tony Lim <tonyrlim97@gmail.com>
2020-06-18 18:15:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//O(1) [Constant Time]. With this algorithm, it does not matter the size of the array, the number of comparisons/operations will always be constant.
+
+const boxes = [0, 1, 2, 3, 4, 5, 6];
+
+function firstBox (array) {
+  console.log(array[0]);
+}
+
+firstBox(boxes);
